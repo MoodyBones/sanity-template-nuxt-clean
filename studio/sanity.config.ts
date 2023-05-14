4f926df16210a65442b4e5743805f8efea677ed7
Mel <jones.mel@gmail.com>
2023-05-14 15:10:01 +0200

    feat: add project name

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -7,8 +7,8 @@ export const projectId = process.env.SANITY_STUDIO_PROJECT_ID!
 export const dataset = process.env.SANITY_STUDIO_DATASET!
 
 export default defineConfig({
-  name: 'project-name',
-  title: 'Project Name',
+  name: 'positive-boop',
+  title: 'Positive Boop',
   projectId,
   dataset,
   plugins: [deskTool(), visionTool()],
